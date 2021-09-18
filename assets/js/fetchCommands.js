499f94dbc7750f6eeed13a1142ee5855a1c20a8e
LawTRIBAL <65894260+LawTRIBAL-Official@users.noreply.github.com>
2021-09-18 16:36:19 +0200

    Fetch
    Trying out another fetch method for .json on GitHub Pages

--- assets/js/fetchCommands.js
+++ assets/js/fetchCommands.js
@@ -1,4 +1,4 @@
-fs.readFileSync("./assets/data/commandsdb.json")
+fetch("https://github.com/LawTRIBAL-Official/Dungeon-Raiders.doc.github.io/blob/main/assets/data/commandsdb.json")
     .then(response => response.json())
     .then(data => {
         console.log(data.commands);
