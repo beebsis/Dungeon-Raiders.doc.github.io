0e28dd6a0a4b4de0c40c39a7ef2e474d00471609
LawTRIBAL-Official <65894260+LawTRIBAL-Official@users.noreply.github.com>
2021-10-01 14:01:54 +0200

    Item .json
    The JavaScript works to implement the items. HOWEVER, it's poorly optimized and I'll be changing it soon... A poorly quickfix.

--- js/items.js
+++ js/items.js
@@ -1,101 +1,254 @@
 let glovesList = '{ "gloves" : [' +
-'{ "itemTier":"T1" , "itemBuff":"Increase Damage" },' +
-'{ "itemTier":"T2" , "itemBuff":"Increase Damage" }  ,' +
-'{ "itemTier":"T3" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T4" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T5" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T6" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T7" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T8" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T9" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T10" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T11" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T12" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T13" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T14" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T15" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T16" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T17" , "itemBuff":"Increase Damage" } ,' +
-'{ "itemTier":"T18" , "itemBuff":"Increase Damage" } ]}'; 
+'{ "itemTier":"T0" , "itemBuff":"Buffs Damage" },' +
+'{ "itemTier":"T1" , "itemBuff":"Buffs Damage" },' +
+'{ "itemTier":"T2" , "itemBuff":"Buffs Damage" }  ,' +
+'{ "itemTier":"T3" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T4" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T5" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T6" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T7" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T8" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T9" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T10" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T11" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T12" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T13" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T14" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T15" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T16" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T17" , "itemBuff":"Buffs Damage" } ,' +
+'{ "itemTier":"T18" , "itemBuff":"Buffs Damage" } ]}'; 
 
 let bootsList = '{ "boots" : [' +
-'{ "itemTier":"T1" , "itemBuff":"Increase Max HP" },' +
-'{ "itemTier":"T2" , "itemBuff":"Increase Max HP" }  ,' +
-'{ "itemTier":"T3" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T4" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T5" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T6" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T7" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T8" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T9" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T10" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T11" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T12" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T13" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T14" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T15" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T16" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T17" , "itemBuff":"Increase Max HP" } ,' +
-'{ "itemTier":"T18" , "itemBuff":"Increase Max HP" } ]}'; 
+'{ "itemTier":"T0" , "itemBuff":"Buffs Max HP" },' +
+'{ "itemTier":"T1" , "itemBuff":"Buffs Max HP" },' +
+'{ "itemTier":"T2" , "itemBuff":"Buffs Max HP" }  ,' +
+'{ "itemTier":"T3" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T4" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T5" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T6" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T7" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T8" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T9" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T10" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T11" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T12" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T13" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T14" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T15" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T16" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T17" , "itemBuff":"Buffs Max HP" } ,' +
+'{ "itemTier":"T18" , "itemBuff":"Buffs Max HP" } ]}'; 
 
 let beltsList = '{ "belts" : [' +
-'{ "itemTier":"T1" , "itemBuff":"Increase Armor" },' +
-'{ "itemTier":"T2" , "itemBuff":"Increase Armor" }  ,' +
-'{ "itemTier":"T3" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T4" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T5" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T6" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T7" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T8" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T9" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T10" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T11" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T12" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T13" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T14" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T15" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T16" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T17" , "itemBuff":"Increase Armor" } ,' +
-'{ "itemTier":"T18" , "itemBuff":"Increase Armor" } ]}'; 
+'{ "itemTier":"T0" , "itemBuff":"Buffs Armor" },' +
+'{ "itemTier":"T1" , "itemBuff":"Buffs Armor" },' +
+'{ "itemTier":"T2" , "itemBuff":"Buffs Armor" }  ,' +
+'{ "itemTier":"T3" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T4" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T5" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T6" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T7" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T8" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T9" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T10" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T11" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T12" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T13" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T14" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T15" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T16" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T17" , "itemBuff":"Buffs Armor" } ,' +
+'{ "itemTier":"T18" , "itemBuff":"Buffs Armor" } ]}'; 
 
 let ringsList = '{ "rings" : [' +
-'{ "itemTier":"T1" , "itemBuff":"Increase Fury gain" },' +
-'{ "itemTier":"T2" , "itemBuff":"Increase Fury gain" }  ,' +
-'{ "itemTier":"T3" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T4" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T5" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T6" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T7" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T8" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T9" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T10" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T11" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T12" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T13" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T14" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T15" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T16" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T17" , "itemBuff":"Increase Fury gain" } ,' +
-'{ "itemTier":"T18" , "itemBuff":"Increase Fury gain" } ]}'; 
+'{ "itemTier":"T0" , "itemBuff":"Buffs Fury gain" },' +
+'{ "itemTier":"T1" , "itemBuff":"Buffs Fury gain" },' +
+'{ "itemTier":"T2" , "itemBuff":"Buffs Fury gain" }  ,' +
+'{ "itemTier":"T3" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T4" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T5" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T6" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T7" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T8" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T9" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T10" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T11" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T12" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T13" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T14" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T15" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T16" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T17" , "itemBuff":"Buffs Fury gain" } ,' +
+'{ "itemTier":"T18" , "itemBuff":"Buffs Fury gain" } ]}'; 
 
 const gloves = JSON.parse(glovesList);
 const boots = JSON.parse(bootsList);
 const belts = JSON.parse(beltsList);
 const rings = JSON.parse(ringsList);
 
-let i = 1;
+let rows = ['glove-row','boot-row','belt-row','','ring-row'];
+let itemTypeID = ['glove', 'boot', 'belt', 'ring'];
+let tierID = ['gloveTier', 'bootTier', 'beltTier', 'ringTier'];
+let itemStatID = ['gloveStat', 'bootStat', 'beltStat', 'ringStat'];
+let imgLinkItem = ['gloves', 'shoes', 'belt', 'ring'];
+let imgFolder = ['Gloves', 'Boots', 'Belts', 'Rings']
+
+let i = 0; //counter
+let x = 0; //if counter
+let b = 0;
+let c = 0;
+
 
 //Generates items based on json data
-while (i < 18) {
-    let div = document.createElement('div');
-    div.className = 'item-card';
-    let text = document.getElementById("gloves").innerHTML = gloves.gloves[i].itemTier + " " + gloves.gloves[i].itemBuff;
-    div.appendChild(text);
-    document.body.appendChild(div);
+try {
+    
 
-    i++;
+    do {
+        let cardDiv = document.createElement("div");
+        let itemImg = document.createElement("img");
+        let itemTier = document.createElement("h2");
+        let itemStat = document.createElement("p");
+
+        i += 1;
+
+        //GLOVES
+        if (x < 19) {
+
+            itemImg.setAttribute("src", "../assets/img/" + imgFolder[0] + "/spr_" + imgLinkItem[0] + "_" + [x] + ".png");
+    
+            cardDiv.classList.add("item-card");
+            cardDiv.setAttribute("id", itemTypeID[0] + [x]);
+            itemTier.setAttribute("id", tierID[0] + [x])
+            itemStat.setAttribute("id", itemStatID[0] + [x])
+            itemImg.setAttribute("alt", "Tier" + [x])
+            
+            document.getElementById(rows[0]).appendChild(cardDiv);
+            document.getElementById(itemTypeID[0] + [x]).appendChild(itemImg);
+            document.getElementById(itemTypeID[0] + [x]).appendChild(itemTier);
+            document.getElementById(itemTypeID[0] + [x]).appendChild(itemStat);
+
+            document.getElementById(tierID[0] + [x]).innerHTML = gloves.gloves[x].itemTier;
+            document.getElementById(itemStatID[0] + [x]).innerHTML = gloves.gloves[x].itemBuff;
+
+            x++;
+        }
+
+        //BOOTS
+        else if (x > 18 && x < 33) {
+
+            do {
+                let cardDiv = document.createElement("div");
+                let itemImg = document.createElement("img");
+                let itemTier = document.createElement("h2");
+                let itemStat = document.createElement("p");
+
+                itemImg.setAttribute("src", "../assets/img/" + imgFolder[1] + "/spr_" + imgLinkItem[1] + "_" + [b] + ".png");
+        
+                cardDiv.classList.add("item-card");
+                cardDiv.setAttribute("id", itemTypeID[1] + [b]);
+                itemTier.setAttribute("id", tierID[1] + [b])
+                itemStat.setAttribute("id", itemStatID[1] + [b])
+                itemImg.setAttribute("alt", "Tier" + [b])
+                
+                document.getElementById(rows[1]).appendChild(cardDiv);
+                document.getElementById(itemTypeID[1] + [b]).appendChild(itemImg);
+                document.getElementById(itemTypeID[1] + [b]).appendChild(itemTier);
+                document.getElementById(itemTypeID[1] + [b]).appendChild(itemStat);
+
+                document.getElementById(tierID[1] + [b]).innerHTML = boots.boots[b].itemTier;
+                document.getElementById(itemStatID[1] + [b]).innerHTML = boots.boots[b].itemBuff;
+
+                b++;
+                console.log("b: " + b);
+
+            }
+
+            while (b <= x)
+
+        } else if (x > 32 && x < 51) {
+
+            do {
+                let cardDiv = document.createElement("div");
+                let itemImg = document.createElement("img");
+                let itemTier = document.createElement("h2");
+                let itemStat = document.createElement("p");
+
+                itemImg.setAttribute("src", "../assets/img/" + imgFolder[2] + "/spr_" + imgLinkItem[2] + "_" + [c] + ".png");
+        
+                cardDiv.classList.add("item-card");
+                cardDiv.setAttribute("id", itemTypeID[2] + [c]);
+                itemTier.setAttribute("id", tierID[2] + [c])
+                itemStat.setAttribute("id", itemStatID[2] + [c])
+                itemImg.setAttribute("alt", "Tier" + [c])
+                
+                document.getElementById(rows[2]).appendChild(cardDiv);
+                document.getElementById(itemTypeID[2] + [c]).appendChild(itemImg);
+                document.getElementById(itemTypeID[2] + [c]).appendChild(itemTier);
+                document.getElementById(itemTypeID[2] + [c]).appendChild(itemStat);
+
+                document.getElementById(tierID[2] + [c]).innerHTML = belts.belts[c].itemTier;
+                document.getElementById(itemStatID[2] + [c]).innerHTML = belts.belts[c].itemBuff;
+
+                c++;
+                console.log("c: " + c);
+
+            }
+
+            while (c <= x)
+            
+        }
+        else {
+            console.log("else statement");
+        }
+
+    }
+    while (i <= x)
+
+
+
+
+} catch(err) {
+    console.log(err)
 }
 
+
+
+
+/*
+
+    let cardDiv = document.createElement("div");
+    let itemImg = document.createElement("img");
+    let itemTier = document.createElement("h2");
+    let itemStat = document.createElement("p");
+    
+    //        itemImg.setAttribute("src", "../assets/img/Gloves/spr_gloves_" + [i] + ".png");
+        itemImg.setAttribute("src", "../assets/img/" + imgFolder[0] + "/spr_" + imgLinkItem[0] + "_" + [i] + ".png");
+    
+        cardDiv.classList.add("item-card");
+        cardDiv.setAttribute("id", itemTypeID[0] + [i]);
+        itemTier.setAttribute("id", tierID[0] + [i])
+        itemStat.setAttribute("id", itemStatID[0] + [i])
+        itemImg.setAttribute("alt", "Tier" + [i])
+        
+        document.getElementById(rows[x]).appendChild(cardDiv);
+        document.getElementById(itemTypeID[0] + [i]).appendChild(itemImg);
+        document.getElementById(itemTypeID[0] + [i]).appendChild(itemTier);
+        document.getElementById(itemTypeID[0] + [i]).appendChild(itemStat);
+
+        document.getElementById(tierID[0] + [i]).innerHTML = gloves.gloves[i].itemTier;
+        document.getElementById(itemStatID[0] + [i]).innerHTML = gloves.gloves[i].itemBuff;
+
+        console.log(itemImg);
+        console.log(x);
+*/
+
+/*
+
+    let itemTier = document.getElementById("gloveTier").innerHTML = gloves.gloves[i].itemTier;
+    let statWeight = document.getElementById("gloveStat").innerHTML = gloves.gloves[i].itemBuff
+
+*/
 
 /*
 
