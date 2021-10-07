7cb47e8be0a603c04f59497f62d2eebf91238a91
LawTRIBAL-Official <65894260+LawTRIBAL-Official@users.noreply.github.com>
2021-10-07 15:28:28 +0200

    pain

--- js/items2.js
+++ js/items2.js
@@ -1,56 +1,75 @@
-const myItems = {
-    itemTier1: [
-        {
-            helmet: [
-                {
-                    title: "helmet T1",
-                    buff: "armor buff",
-                    stats: "+6.8"
-                }
-            ],
-            boots: [
-                {
-                    title: "helmet T1",
-                    buff: "armor buff",
-                    stats: "+6.8"
-                }
-            ]
+const data = {
+    tier1: {
+        id: 1,
+        type: "helmet1",
+        stats: {
+            type1: "Armor buff",
+            type2: "test"
+        },
+        
+        id: 2,
+        type: "vest1",
+        stats: {
+            type1: "Const buff",
+            type2: "test"
+        },
+
+        id: 3,
+        type: "belt1",
+        stats: {
+            type1: "Dex buff",
+            type2: "test"
         }
-    ],
-
-    itemTier2: [
-        {
-            helmet: [
-                {
-                    title: "helmet T1",
-                    buff: "armor buff",
-                    stats: "+6.8"
-                }
-            ],
-            boots: [
-                {
-                    title: "helmet T1",
-                    buff: "armor buff",
-                    stats: "+6.8"
-                }
-            ]
+    },
+    tier2: {
+        id: 1,
+        type: "helmet2",
+        stats: {
+            type1: "Armor buff2",
+            type2: "test2"
         }
-    ]
-};
+    }
+}
 
 
-    //const myItemObj = JSON.stringify(myItems);
-    //const myObj = JSON.parse (myItemObj);
-
-    try {
 
 
-        //logs specific item
-        //console.log(myItems.itemTier1.map(x => x.helmet))
+try {
 
-        //Logs list of item in tier1
-        console.log(myItems.itemTier1.map(x => x))
-        console.log(myItems.itemTier2.map(x => x))
+/*
+console.log(data.tier.map(x => x))
+const items = data.tier[0];
+console.log(items);
+*/
+
+
+const isObject = function(val) {
+    if (val === null) {
+        return false;
+    }
+    return (typeof val === 'object')
+}
+
+//itterating through data props
+const objProps = function(obj) {
+    for ( let val in obj ) {
+        if (isObject(obj[val])) {
+            objProps(obj[val]);
+        } 
+        else {
+            console.log("val: " + val, obj[val]);
+        }
+    };
+}
+
+objProps(data);
+
+
+
+// Complicated nesting frustration INSERT HERE REEEEEEEEEEEEEEEEEE
+
+
+
 
 
         
